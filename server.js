ec48dfed62275ee0434e913a8a33242357f9545e
meetgami510 <meetgami105@gmail.com>
2023-01-25 17:53:26 +0530

    created fronted and backed with link with bootstrap

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const colors = require('colors')
+const moragan = require('morgan')
+const dotenv = require('dotenv')
+
+
+//Need to put dotenv config function
+dotenv.config()
+
+//rest Object
+const app = express()
+
+//middlewares
+//its handles the error related when we pass the object in form of json
+app.use(express.json())
+app.use(moragan('dev'))
+
+//routes
+app.get('/', (req,res)=>{
+    res.status(200).send({
+        message:"server running"
+    })
+})
+
+//decleareing port
+const port = process.env.PORT || 8080
+
+//we need to require listen
+app.listen(port,() => {
+    console.log(
+        `Server is running in ${process.env.NODE_MODE} on port ${process.env.PORT}`.bgCyan.white
+    )
+})
+
