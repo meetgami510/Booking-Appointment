4974450751c78d1cf77d9b03c83ed0fe1253d970
meetgami510 <meetgami105@gmail.com>
2023-01-26 16:45:44 +0530

    login form UI is created

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import {Button, Form, Input }  from 'antd'
+import "../styles/RegisterStyles.css"
+import {Link} from 'react-router-dom'
 
 function Login() {
+  const onfinishHandler = (values)=> {
+    console.log(values);
+  }
   return (
     <div>
-      <h1> Login pages</h1>
+      <div className="form-container">
+            <Form layout='vertical' onFinish={onfinishHandler} className="register-form">
+                <h3 className='text-center'>Login From</h3>
+                <Form.Item label="Email" name="email">
+                    <Input type='email' required/>
+                </Form.Item>
+                <Form.Item label="Password" name="password">
+                    <Input type='password' required/>
+                </Form.Item>
+                <Link to='/register' className='m-2'>
+                    Not a User Register here
+                </Link>
+                <button className='btn btn-primary' type='submit'> Login </button>
+            </Form>
+        </div>
     </div>
   )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {Button, Form, Input }  from 'antd'
 import "../styles/RegisterStyles.css"
+import {Link} from 'react-router-dom'
 function Register() {
-
+   
     const onfinishHandler = (values)=> {
         console.log(values);
     }
@@ -10,7 +11,7 @@ function Register() {
     <div>   
       <div className="form-container">
         <Form layout='vertical' onFinish={onfinishHandler} className="register-form">
-            <h3>Register From</h3>
+            <h3 className='text-center'>Register From</h3>
             <Form.Item label="Name" name="name">
                 <Input type='text' required/>
             </Form.Item>
@@ -20,6 +21,9 @@ function Register() {
             <Form.Item label="Password" name="password">
                 <Input type='password' required/>
             </Form.Item>
+            <Link to='/login' className='m-2'>
+                Already User Login here 
+            </Link>
             <button className='btn btn-primary' type='submit'> Register </button>
         </Form>
       </div>
